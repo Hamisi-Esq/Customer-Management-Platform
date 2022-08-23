7ea0092ab5229accbf38e932f6775574cb960060
Hamisi-Esq <brivahamisi@gmail.com>
2022-08-23 22:57:51 +0300

    Latest Updates

--- crm/urls.py
+++ crm/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from xml.etree.ElementInclude import include
 from django.contrib import admin
+
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+
     path('', include('accounts.urls')),
 ]
